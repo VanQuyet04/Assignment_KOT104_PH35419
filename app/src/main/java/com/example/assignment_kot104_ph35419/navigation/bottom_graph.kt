0266bd40ac2bed9464a224600301b1640b68501b
BuiCuong <truongquyet137@gmail.com>
2024-05-26 10:05:17 +0700

    update nav

--- app/src/main/java/com/example/assignment_kot104_ph35419/navigation/bottom_graph.kt
+++ app/src/main/java/com/example/assignment_kot104_ph35419/navigation/bottom_graph.kt
@@ -1,6 +1,14 @@
 package com.example.assignment_kot104_ph35419.navigation
 
+import CartScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -18,6 +26,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.assignment_kot104_ph35419.R
 import com.example.assignment_kot104_ph35419.ui.screens.FavouriteScreen
 import com.example.assignment_kot104_ph35419.ui.screens.HomeScreen
@@ -33,14 +42,15 @@ fun bottom_graph(navController: NavHostController) {
         composable("favourite") { FavouriteScreen(navController) }
         composable("profile") { ProfileScreen(navController) }
 
+        composable("cart") { CartScreen(navController) }
+
+
     }
 }
 
 
 @Composable
-fun BottomNavigationBar(nav: NavController) {
-    var selectedItem by remember { mutableStateOf("home") }
-
+fun BottomNavigationBar(navController: NavController) {
     val items = listOf(
         "home" to R.drawable.ic_home,
         "favourite" to R.drawable.ic_favourite,
@@ -48,39 +58,53 @@ fun BottomNavigationBar(nav: NavController) {
         "profile" to R.drawable.ic_profile
     )
 
-    NavigationBar(containerColor = Color.White) {
-        items.forEach { (name, icon) ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        painter = painterResource(id = icon),
-                        contentDescription = name.replaceFirstChar { it.uppercase() },
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                selected = selectedItem == name,
-                onClick = {
-                    selectedItem = name
-                    nav.navigate(name) {
-                        //Xóa tất cả các trang đich trước để tránh chồng chất trang
-                        popUpTo(nav.graph.startDestinationId) {
-                            saveState = true
-                        }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
-                        // Điều hướng đến 1 màn hình duy nhất và k tạo thêm bản sao
-                        launchSingleTop = true
-
-                        // Khôi phục trạng thái đã lưu
-                        restoreState = true
-                    }
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = Color.Black,
-                    unselectedIconColor = Color.Gray,
-                    indicatorColor = Color.White
-                ),
-                alwaysShowLabel = false,
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp) // Chiều cao của thanh điều hướng
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(35.dp) // Điều chỉnh giá trị này để bo góc
+            ) .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(20.dp)
             )
+    ) {
+        NavigationBar(containerColor = Color.White) {
+            items.forEach { (name, icon) ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = icon),
+                            contentDescription = name.replaceFirstChar { it.uppercase() },
+                            modifier = Modifier.size(24.dp)
+                        )
+                    },
+                    selected = currentRoute == name,
+                    onClick = {
+                        navController.navigate(name) {
+                            // Xóa tất cả các trang đích trước để tránh chồng chất trang
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            // Điều hướng đến 1 màn hình duy nhất và không tạo thêm bản sao
+                            launchSingleTop = true
+                            // Khôi phục trạng thái đã lưu
+                            restoreState = true
+                        }
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = Color.Black,
+                        unselectedIconColor = Color.Gray,
+                        indicatorColor = Color.White
+                    ),
+                    alwaysShowLabel = false,
+                )
+            }
         }
     }
 }
