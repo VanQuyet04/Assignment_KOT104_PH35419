2e3f056dc5895fa1b7d110fa633244c62bb5d2a9
BuiCuong <truongquyet137@gmail.com>
2024-05-26 08:53:11 +0700

    update nav

--- app/src/main/java/com/example/assignment_kot104_ph35419/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/assignment_kot104_ph35419/ui/screens/DetailScreen.kt
@@ -15,11 +15,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.assignment_kot104_ph35419.R
 
 @Composable
